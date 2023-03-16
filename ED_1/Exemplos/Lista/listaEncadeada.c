813250048ea714c4bc3529751f91818e26a94a3d
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2023-03-15 22:40:58 -0300

    update

--- ED_1/Exemplos/Lista/listaEncadeada.c
+++ ED_1/Exemplos/Lista/listaEncadeada.c
@@ -16,7 +16,7 @@ typedef struct linked_list
 } LinkedList;
 
 // Função que adiciona um novo nó ao final da lista encadeada
-void add_node(LinkedList *list, int data)
+void insertAtEnd(LinkedList *list, int data)
 {
     // Aloca dinamicamente um novo nó
     Node *new_node = (Node *)malloc(sizeof(Node));
@@ -42,10 +42,126 @@ void add_node(LinkedList *list, int data)
     }
 }
 
+// Função que adiciona um novo nó ao início da lista encadeada
+void insertAtStart(LinkedList *list, int data)
+{
+    // Aloca dinamicamente um novo nó
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    new_node->data = data;
+
+    // Verifica se a lista encadeada está vazia
+    if (list->head == NULL)
+    {
+        // Se estiver vazia, o novo nó é o nó cabeça
+        new_node->next = NULL;
+        list->head = new_node;
+    }
+    else
+    {
+        // Se não estiver vazia, o novo nó aponta para o nó cabeça atual
+        new_node->next = list->head;
+        // O nó cabeça da lista passa a ser o novo nó
+        list->head = new_node;
+    }
+}
+
+void insertAtPosition(LinkedList *list, int data, int position)
+{
+    // Aloca dinamicamente um novo nó
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    new_node->data = data;
+
+    // Verifica se a lista encadeada está vazia
+    if (list->head == NULL)
+    {
+        // Se estiver vazia, o novo nó é o nó cabeça
+        new_node->next = NULL;
+        list->head = new_node;
+    }
+    else
+    {
+        // Se não estiver vazia, o novo nó aponta para o nó cabeça atual
+        Node *current = list->head;
+        Node *previous = NULL;
+        int i = 0;
+        while (current->next != NULL && i < position)
+        {
+            previous = current;
+            current = current->next;
+            i++;
+        }
+        previous->next = new_node;
+        new_node->next = current;
+    }
+}
+
+void remeveAtStart(LinkedList *list)
+{
+    // Verifica se a lista encadeada está vazia
+    if (list->head == NULL)
+    {
+        printf("Lista vazia");
+    }
+    else
+    {
+        // Se não estiver vazia, o novo nó aponta para o nó cabeça atual
+        Node *current = list->head;
+        list->head = current->next;
+        free(current);
+    }
+}
+
+void removeAtEnd(LinkedList *list)
+{
+    // Verifica se a lista encadeada está vazia
+    if (list->head == NULL)
+    {
+        printf("Lista vazia");
+    }
+    else
+    {
+        // Se não estiver vazia, o novo nó aponta para o nó cabeça atual
+        Node *current = list->head;
+        Node *previous = NULL;
+        while (current->next != NULL)
+        {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = NULL;
+        free(current);
+    }
+}
+
+void removeAtPosition(LinkedList *list, int position)
+{
+    // Verifica se a lista encadeada está vazia
+    if (list->head == NULL)
+    {
+        printf("Lista vazia");
+    }
+    else
+    {
+        // Se não estiver vazia, o novo nó aponta para o nó cabeça atual
+        Node *current = list->head;
+        Node *previous = NULL;
+        int i = 0;
+        while (current->next != NULL && i < position)
+        {
+            previous = current;
+            current = current->next;
+            i++;
+        }
+        previous->next = current->next;
+        free(current);
+    }
+}
+
 // Função que imprime todos os elementos da lista encadeada
 void print_list(LinkedList *list)
 {
     Node *current = list->head;
+    printf("Lista: ");
     while (current != NULL)
     {
         printf("%d ", current->data);
@@ -62,11 +178,21 @@ int main()
     list.head = NULL;
 
     // Adiciona três nós à lista
-    add_node(&list, 1);
-    add_node(&list, 2);
-    add_node(&list, 3);
+    insertAtEnd(&list, 1);
+    insertAtEnd(&list, 2);
+    insertAtEnd(&list, 3);
+    insertAtStart(&list, 0);
+    insertAtStart(&list, 4);
+    insertAtStart(&list, 5);
+    insertAtPosition(&list, 6, 2);
 
     // Imprime todos os nós da lista
+    print_list(&list);
+
+    remeveAtStart(&list);
+    removeAtEnd(&list);
+    removeAtPosition(&list, 3);
+
     print_list(&list);
 
     return 0;
