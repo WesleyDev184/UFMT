b875b2ba1ee02eaeeb91148bc6afa0a8c2d43487
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2023-05-05 20:45:30 -0300

    update

--- ED_1/Trabalhos/Trabalho3/JogoDeTabuleiro/Jogador.c
+++ ED_1/Trabalhos/Trabalho3/JogoDeTabuleiro/Jogador.c
@@ -1,35 +1,102 @@
+#ifndef JOGADORES_C
+#define JOGADORES_C
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Definição da estrutura de nó
-struct node
+typedef struct nodePlayer
 {
+    char id;
     int data;
-    struct node *next;
-};
+    struct nodePlayer *next;
+    // struct node *prev;
+} NodePlayer;
 
-// Função para inserir um novo elemento na lista circular
-void insert(struct node **head, int data)
+typedef struct listPlayers
 {
-    // Aloca um novo nó
-    struct node *new_node = (struct node *)malloc(sizeof(struct node));
-    new_node->data = data;
+    NodePlayer *head;
+    NodePlayer *rear;
+} ListPlayers;
 
-    // Se a lista estiver vazia, o novo nó se torna o primeiro e único elemento
-    if (*head == NULL)
+/**
+ * This function creates a circular doubly linked list with nodes containing data and an ID.
+ *
+ * @param size The size parameter is an integer that represents the number of elements in the linked
+ * list that will be created.
+ * @param values An array of integers containing the values to be stored in the linked list.
+ *
+ * @return The function `createList` returns a pointer to a `LinkedList` structure.
+ */
+ListPlayers *createPlayerList(int size, char **values)
+{
+    ListPlayers *l = (ListPlayers *)malloc(sizeof(ListPlayers));
+    l->head = NULL;
+    l->rear = NULL;
+
+    NodePlayer *new_node = NULL;
+    for (int i = 0; i < size; i++)
     {
-        *head = new_node;
-        new_node->next = *head;
+        new_node = (NodePlayer *)malloc(sizeof(NodePlayer));
+        new_node->id = (char *)malloc(strlen(values[i]) + 1); // Aloca espaço para a string, incluindo o caractere terminador
+        new_node->data = NULL;                                // Define o campo 'data' como um ponteiro e atribui o valor NULL
+        new_node->next = NULL;
+
+        if (l->head == NULL)
+        {
+            // Se estiver vazia, o novo nó é o nó cabeça
+            l->head = new_node;
+            l->rear = new_node;
+        }
+        else
+        {
+            // Se não estiver vazia, o novo nó aponta para o nó cabeça atual
+            new_node->next = l->head;
+
+            l->head = new_node;
+        }
     }
-    // Caso contrário, insere o novo nó no final da lista
+
+    l->rear->next = l->head;
+    return l;
+}
+
+void destroyPlayerList(ListPlayers *list)
+{
+    // Percorre a lista e libera a memória alocada para cada nó e para cada string no campo 'id'
+    NodePlayer *current = list->head;
+    while (current != NULL)
+    {
+        NodePlayer *next = current->next;
+        free(current->id);
+        free(current);
+        current = next;
+    }
+
+    // Libera a memória alocada para a estrutura da lista
+    free(list);
+}
+
+void printListPlayer(ListPlayers *list)
+{
+    // Verifica se a lista encadeada dupla está vazia
+    if (list->head == NULL)
+    {
+        printf("Lista vazia :(");
+    }
     else
     {
-        struct node *temp = *head;
-        while (temp->next != *head)
+        // Se não estiver vazia, percorre a lista até encontrar o último nó
+        printf("Lista: ");
+        NodePlayer *current = list->head;
+        while (current != list->rear)
         {
-            temp = temp->next;
+            printf("%s ", current->id);
+            current = current->next;
         }
-        temp->next = new_node;
-        new_node->next = *head;
+        printf("%s ", current->id);
     }
+    printf("\n");
 }
+
+#endif
--- ED_1/Trabalhos/Trabalho3/JogoDeTabuleiro/main.c
+++ ED_1/Trabalhos/Trabalho3/JogoDeTabuleiro/main.c
@@ -1,19 +1,20 @@
 /*
  * UFMT - Ciencia da Computacao
- * Estrutura de Dados 1 - Trabalho 6 : lst - estatica x dinamica
+ * Estrutura de Dados 1 - Trabalho 6 : lst - estática x dinamica
  * Prof Ivairton
  * Discentes:  Lucas Barbosa Amaral, Wesley Antonio Junior dos Santos
  */
-
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "Tabuleiro.c"
+#include "Jogador.c"
 
-LinkedList *createCircularList(char *filename)
+LinkedList *createCircularBoard(char *filename)
 {
     FILE *fp;
     int size, value, values[26];
-    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList)) ;
+    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
 
     fp = fopen(filename, "r");
     if (fp == NULL)
@@ -41,12 +42,55 @@ LinkedList *createCircularList(char *filename)
     return list;
 }
 
+ListPlayers *createCircularListPlayers(char *filename)
+{
+    FILE *fp;
+    int size;
+    char players[100][100];
+    ListPlayers *listOfPlayers = (ListPlayers *)malloc(sizeof(ListPlayers));
+
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return NULL;
+    }
+
+    // Lê o tamanho da lista na primeira linha do arquivo
+    fscanf(fp, "%d", &size);
+    printf("Tamanho da lista: %d\n", size);
+
+    // Lê os valores do arquivo e adiciona na lista
+    for (int i = 0; i < size; i++)
+    {
+        fscanf(fp, "%s", players[i]);
+        printf("Player %d: %s\n", i, players[i]);
+    }
+
+    listOfPlayers = createPlayerList(size, (char *)players);
+
+    fclose(fp);
+
+    return listOfPlayers;
+}
+
 int main()
 {
-    LinkedList *list = createCircularList("defineBoard.txt");
+    /*
 
-   
-   printList(list);
+    int num;
+
+    srand(time(NULL));
+
+    num = rand() % 6 + 1;
+    */
+    LinkedList *board = createCircularBoard("defineBoard.txt");
+
+    printList(board);
+
+    ListPlayers *players = createCircularListPlayers("definePlayer.txt");
+
+    // printListPlayer(players);
 
     return 0;
 }
