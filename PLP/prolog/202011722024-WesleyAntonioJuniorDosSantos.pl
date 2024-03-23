27b7e5a00d2a129d907e051c3ca12e7b880d38d8
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2024-03-23 00:44:13 -0300

    Update 202011722024-WesleyAntonioJuniorDosSantos.pl

--- PLP/prolog/202011722024-WesleyAntonioJuniorDosSantos.pl
+++ PLP/prolog/202011722024-WesleyAntonioJuniorDosSantos.pl
@@ -26,6 +26,11 @@ familia(
         pessoa(rickon,stark,data(29,maio,1999),morto)
     ]
 ).
+familia(
+    pessoa(tony,rogers,data(1,janeiro,1955),empregado(abb,1200)),
+    pessoa(ana,rogers,data(1,maio,1955),desempregado),
+    []
+).
 
 membro(X,[X|C]).
 membro(X, [Y|C]):- membro(X,C).
@@ -43,13 +48,53 @@ data(D) :- existe(X), dataNascimento(X, D).
 salario(pessoa(_,_,_,empregado(_,S)),S).
 salario(pessoa(_,_,_,desempregado),0).
 
-primeiro_nome(pessoa(Nome, _, _, _), Nome).
-sobrenome(pessoa(_, Sobrenome, _, _), Sobrenome).
-
 imprimir_nome_completo(Nome, Sobrenome) :- write(Nome), write(' '), write(Sobrenome), nl.
 
-nomes(X) :- marido(X), primeiro_nome(X, Nome), sobrenome(X, Sobrenome), imprimir_nome_completo(Nome, Sobrenome).
-nomes(X) :- esposa(X), primeiro_nome(X, Nome), sobrenome(X, Sobrenome), imprimir_nome_completo(Nome, Sobrenome).
-nomes(X) :-familia(_, _, Filhos),membro(Pessoa, Filhos),primeiro_nome(Pessoa, X),sobrenome(Pessoa, Sobrenome),imprimir_nome_completo(X, Sobrenome).
+nomes(X) :- 
+    familia(pessoa(X,S,_,_),_,_),
+    imprimir_nome_completo(X, S).
 
-filhosBydt(Filho, Ano) :- filho(pessoa(Filho, _, data(_, _, Ano), _)).
+nomes(X) :- 
+    familia(_,pessoa(X,S,_,_),_), 
+    imprimir_nome_completo(X, S).
+
+nomes(X) :-
+    familia(_, _, Filhos),
+    membro(pessoa(X,S,_,_), Filhos),
+    imprimir_nome_completo(X, S).
+
+filhosBydt(Filho, Ano) :- filho(pessoa(Filho, S, data(_, _, Ano), _)),imprimir_nome_completo(Filho,S).
+
+esposaEmpregada(X) :- familia(_, pessoa(X, Sobrenome, _, empregado(_, _)), _),imprimir_nome_completo(X,Sobrenome).
+
+pessoasDesempregada(X,D) :- familia(pessoa(X,S,data(_,_,Ano),desempregado),_,_),Ano < D,imprimir_nome_completo(X,S).
+pessoasDesempregada(X,D) :- familia(_,pessoa(X,S,data(_,_,Ano),desempregado),_),Ano < D,imprimir_nome_completo(X,S).
+pessoasDesempregada(X,D):- familia(_,_,Filhos),membro(pessoa(X,S,data(_,_,Ano),desempregado),Filhos),Ano < D,imprimir_nome_completo(X,S).
+
+nascidoAntesSalario(X,A,S):- familia(pessoa(X,S,data(_,_,Ano),empregado(_,Salario)),_,_),Ano < A,Salario < S,imprimir_nome_completo(X,S).
+nascidoAntesSalario(X,A,S):- familia(_,pessoa(X,S,data(_,_,Ano),empregado(_,Salario)),_),Ano < A,Salario < S,imprimir_nome_completo(X,S).
+nascidoAntesSalario(X,A,S):- familia(_,_,Filhos),membro(pessoa(X,S,data(_,_,Ano),empregado(_,Salario)),Filhos),Ano < A,Salario < S,imprimir_nome_completo(X,S). 
+
+familiaMinimFilhos(X,Min):- familia(_,pessoa(_,X,_,_),Filhos),length(Filhos, T),T >= Min,write(X).
+
+rendaFamilia(Sobrenome, RendaTotal) :-
+    familia(_, pessoa(_, Sobrenome, _, _), Filhos),
+    calcularRenda(Filhos, RendaTotal).
+
+calcularRenda([], 0).
+calcularRenda([pessoa(_, _, _, empregado(_, Salario))|T], RendaTotal) :-
+    calcularRenda(T, RendaRestante),
+    RendaTotal is Salario + RendaRestante.
+calcularRenda([pessoa(_, _, _, desempregado)|T], RendaTotal) :-
+    calcularRenda(T, RendaTotal).
+calcularRenda([pessoa(_, _, _, morto)|T], RendaTotal) :-
+    calcularRenda(T, RendaTotal).
+
+familiaSemFilhos(Sobrenome):-
+    familia(_, pessoa(_, Sobrenome, _, _), Filhos),
+    length(Filhos, 0),
+    write(Sobrenome).
+
+filhosDesempregados(X) :- familia(_,_,Filhos),membro(pessoa(X,S,_,desempregado),Filhos),imprimir_nome_completo(X,S).
+
+paisIdade(X,D):- familia(pessoa(_,_,data(_,_,AnoPai),_),pessoa(_,_,data(_,_,AnoMae),_),Filhos),membro(pessoa(X,S,_,_),Filhos),T is abs(AnoPai - AnoMae),T >= D,imprimir_nome_completo(X,S).
