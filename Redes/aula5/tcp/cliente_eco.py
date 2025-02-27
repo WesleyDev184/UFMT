89ed03be4048826c78f777dfc039b4cb86e4be04
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2025-02-27 15:20:23 -0300

    up

---
+++ Redes/aula5/tcp/cliente_eco.py
@@ -0,0 +1,26 @@
+from socket import *
+import sys
+
+if len(sys.argv) < 3:
+  print(f"Uso: python3 {sys.argv[0]} <ip_servidor> <porta_servidor>")
+  sys.exit(1)
+
+server_addr = (sys.argv[1], int(sys.argv[2]))
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.connect(server_addr) # Conecta-se ao servidor
+
+print(f"Conectando-se a {server_addr}")
+
+try:
+  msg = input("Digite uma mensagem: ")
+  sock.sendto(msg.encode(), server_addr)
+  msg, addr = sock.recvfrom(1024)
+  print(f"Recebido de {addr}: {msg.decode()}")
+except Exception as e:
+  print(f"Erro: {e}")
+except KeyboardInterrupt:
+  print("Cliente encerrado")
+finally:
+  sock.close()
+  print("Conexão encerrada")  
