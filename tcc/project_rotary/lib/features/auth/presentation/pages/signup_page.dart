eede42a1e1c0cc1581b996f2f5768d51d98b148e
WesleyDev184 <wa.wesley01@gmail.com>
2025-04-21 17:47:00 -0300

    up

---
+++ tcc/project_rotary/lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+import '../widgets/signup_form.dart';
+import '../widgets/header_logo.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: primaryColor,
+        image: DecorationImage(
+          image: const AssetImage("assets/images/bg.jpg"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            primaryColor.withOpacity(0.2),
+            BlendMode.dstATop,
+          ),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: const [
+            Positioned(top: 70, left: 0, right: 0, child: HeaderLogo()),
+            Positioned(bottom: 0, left: 0, right: 0, child: SignUpForm()),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ tcc/project_rotary/lib/features/auth/presentation/widgets/signup_form.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:project_rotary/core/components/button.dart';
+import 'package:project_rotary/core/components/input_field.dart';
+import 'package:project_rotary/core/components/password_field.dart';
+
+class SignUpForm extends StatelessWidget {
+  const SignUpForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+    final nameController = TextEditingController();
+    final phoneController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+    final confirmPasswordController = TextEditingController();
+
+    return Card(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Criar conta",
+              style: TextStyle(
+                color: primaryColor,
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            const SizedBox(height: 16),
+            InputField(
+              controller: nameController,
+              hint: "Nome",
+              icon: Icons.person,
+            ),
+
+            const SizedBox(height: 8),
+            InputField(
+              controller: emailController,
+              hint: "Email",
+              icon: Icons.email,
+            ),
+
+            const SizedBox(height: 8),
+            InputField(
+              controller: phoneController,
+              hint: "Telefone",
+              icon: Icons.phone,
+            ),
+            const SizedBox(height: 8),
+
+            PasswordField(controller: passwordController, hint: "Senha"),
+            const SizedBox(height: 8),
+
+            PasswordField(
+              controller: confirmPasswordController,
+              hint: "Confirmar senha",
+            ),
+
+            const SizedBox(height: 16),
+            Button(
+              onPressed: () {
+                debugPrint("Name: ${nameController.text}");
+              },
+              text: "Criar conta",
+              backgroundColor: Colors.green,
+              isFullWidth: true,
+            ),
+
+            const SizedBox(height: 8),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Já tem uma conta? Faça login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
