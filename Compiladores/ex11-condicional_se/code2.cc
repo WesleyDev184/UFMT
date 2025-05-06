9d398098b2868f4fb32f412ea428c104e9b1b22c
Wesley Antonio <wa.wesley01@gmail.com>
2025-05-06 16:13:49 -0300

    up

---
+++ Compiladores/ex11-condicional_se/code2.cc
@@ -0,0 +1,13 @@
+int valor;
+
+write "Informe um valor:";
+read valor;
+
+if (2 - 1 > valor)
+begin
+    write "O valor do teste eh VERDADEIRO!\n";
+end
+else
+begin
+    write "O valor do teste eh FALSO!\n";
+end
